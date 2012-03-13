b43b2839ea94aad31a4823b1fd660ccb6c88b4fd
Kristoffer Sjögren <stoffe@office.deephacks.org>
2012-03-13 01:12:24 +0100

    added support for providing sql statements as argument instead of files

--- support/support-test/src/main/java/org/deephacks/tools4j/support/test/DdlExec.java
+++ support/support-test/src/main/java/org/deephacks/tools4j/support/test/DdlExec.java
@@ -23,8 +23,16 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Properties;
+import java.util.UUID;
 
 public class DdlExec {
+
+    public static void execute(String[] commands, String url, String username, String password,
+            boolean ignoreSqlEx) throws SQLException, IOException {
+        Connection c = getConnection(url, username, password);
+        execute(commands, c, ignoreSqlEx);
+    }
+
     public static void execute(File file, String url, String username, String password,
             boolean ignoreSqlEx) throws SQLException, IOException {
         Connection c = getConnection(url, username, password);
@@ -67,4 +75,71 @@ public class DdlExec {
             }
         }
     }
+
+    private static void execute(String[] commands, Connection c, boolean ignoreSqlEx)
+            throws SQLException, IOException {
+        try {
+            try {
+                for (String sql : commands) {
+                    if (sql == null || "".equals(sql.trim()) || sql.startsWith("--")) {
+                        continue;
+                    }
+                    PreparedStatement stmt = c.prepareStatement(sql);
+                    stmt.execute();
+                }
+            } catch (SQLException e) {
+                if (!ignoreSqlEx) {
+                    throw e;
+                }
+            }
+        } finally {
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        //        drop();
+        //        create();
+        //        insert();
+        execute(new String[] { "SELECT COUNT(*) FROM RELATIONSHIP" },
+                "jdbc:mysql://localhost:3306/stoffe", "stoffe", "stoffe", false);
+
+        System.out.println("start select");
+        long before = System.nanoTime();
+        execute(new String[] { "SELECT * FROM RELATIONSHIP" },
+                "jdbc:mysql://localhost:3306/stoffe", "stoffe", "stoffe", false);
+        System.out.println("select: " + (System.nanoTime() - before) / 1000000.0 + " ms");
+    }
+
+    private static void create() throws SQLException, IOException {
+        String create = "create table RELATIONSHIP (p varchar(40) not null, c varchar(60) not null)";
+        execute(new String[] { create }, "jdbc:mysql://localhost:3306/stoffe", "stoffe", "stoffe",
+                false);
+    }
+
+    private static void insert() throws SQLException, IOException {
+        String[] commands = new String[1000000];
+        //        create();
+        System.out.println("start insert");
+        int id = 0;
+        for (int i = 0; i < commands.length; i++) {
+            commands[i] = "INSERT INTO RELATIONSHIP (p, c) VALUES ('"
+                    + UUID.randomUUID().toString() + "', '" + UUID.randomUUID().toString() + "')";
+        }
+        execute(commands, "jdbc:mysql://localhost:3306/stoffe", "stoffe", "stoffe", false);
+        System.out.println("finish insert");
+    }
+
+    private static void drop() throws SQLException, IOException {
+        String drop = "DROP table RELATIONSHIP";
+        execute(new String[] { drop }, "jdbc:mysql://localhost:3306/stoffe", "stoffe", "stoffe",
+                false);
+    }
+
 }
